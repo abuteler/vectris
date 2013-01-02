09e253973616314475e382935152a49c19a0a00c
Alejandro R. Buteler <alebuteler@gmail.com>
2013-01-01 23:12:46 -0300

    many changes in the design of the program.
    and much progress made, but still a long way to go.

--- js/vectris.js
+++ js/vectris.js
@@ -1,111 +1,291 @@
 var vectris = {
-   grid: {
-      widthInSquares: 10,
-      heightInSquares: 16,
-      squaresMargin: 1,
-      squaresLength:25,
-      getCanvasWidth: function(){
-         return this.widthInSquares*this.getGridUnitLength();
-      },
-      getCanvasHeight: function(){
-         return this.heightInSquares*this.getGridUnitLength();
-      },
-      getGridUnitLength: function(){
-         return this.squaresLength+this.squaresMargin*2;
-      }
-   },
-   init: function() {
-      //show starting section
-      $('#splash').show();
-      //initialize canvas
-      this.initGameCanvas();
-      //initialize menus
-      this.initMainMenu();
-      this.initInGameMenu();
-   },
-   initMainMenu: function() {
-      var self = this;
-      //set hover behaviour
-      $('#main-menu li').mouseover(function() {
-         $(this).toggleClass('hover');
-      });
-      $('#main-menu li').mouseout(function() {
-         $(this).toggleClass('hover');
-      });
-      //set buttons behaviour
-      $('#btn-play').click(function() {
-         $('#splash').hide();
-         $('#game').show();
-         self.renderGrid();
-      });
-   },
-   initInGameMenu: function() {
-      //set hover behaviour
-      $('#in-game-menu li').mouseover(function() {
-         $(this).toggleClass('hover');
-      });
-      $('#in-game-menu li').mouseout(function() {
-         $(this).toggleClass('hover');
-      });
-      //set buttons behaviour
-      $('#btn-quit').click(function() {
-         $('#game').hide();
-         $('#splash').show();
-      });
-   },
-   initGameCanvas: function() {
-      var canvas = $('#game canvas')[0];
-      canvas.width = this.grid.getCanvasWidth();
-      canvas.height = this.grid.getCanvasHeight();
-   },
-   createBlock: function(){
-      var shape = Math.floor(Math.random()),
-         grid = this.grid,
-         startAtX = (grid.widthInSquares/2-1)*grid.getGridUnitLength(),
-         startAtY = grid.squaresMargin,
-         next = grid.squaresLength + grid.squaresMargin*2,
-         block = {
-            squares: [],
-            color: ''
-         };
-      switch (shape) {
-         case 0: //square
-            block.squares.push([startAtX, startAtY]);
-            block.squares.push([startAtX+next, startAtY]);
-            block.squares.push([startAtX, startAtY+next]);
-            block.squares.push([startAtX+next, startAtY+next]);
-            block.color = 'rgb(200,0,200)'
-            break;
-         case 1: //column
-            block.squares.push([startAtX, startAtY]);
-            block.squares.push([startAtX, startAtY+next]);
-            block.squares.push([startAtX, startAtY+next*2]);
-            block.squares.push([startAtX, startAtY+next*3]);
-            block.color = 'rgb(0,200,0)'
-            break;
-      }
-      return block;
-   },
-   renderGrid: function(){
-      var canvas = $('#game canvas')[0],
-         ctx = null,
-         blockSize = this.grid.squaresLength;
-         block = this.createBlock();
-      if (!canvas.getContext){
-         console.error('No canvas support!');
-      } else {
-         //Render
-         ctx = canvas.getContext('2d');
-         ctx.fillStyle = block.color;
-         $.each(block.squares, function(key, square){
-            console.log(blockSize);
-            ctx.fillRect(square[0], square[1], blockSize, blockSize);
-         });
-      }
-   }
+    grid: {
+        widthInSquares: 10,
+        heightInSquares: 16,
+        squaresMargin: 1,
+        squaresLength: 25,
+        theMatrix: [],
+        getCanvasWidth: function() {
+            return this.widthInSquares * this.getGridUnitLength();
+        },
+        getCanvasHeight: function() {
+            return this.heightInSquares * this.getGridUnitLength();
+        },
+        getGridUnitLength: function() {
+            return this.squaresLength + this.squaresMargin * 2;
+        },
+        initializeTheMatrix: function() {
+            var row = [];
+            for(var i = 0; i < this.heightInSquares; i++) {
+                for(var j = 0; j < this.widthInSquares; j++) {
+                    row.push({
+                        occupied: false,
+                        color: undefined
+                    });
+                }
+                this.theMatrix.push(row);
+                row = [];
+            }
+        },
+        updateTheMatrix: function(block) {
+            var self = this;
+            $.each(block.squares, function(key, square) {
+                self.theMatrix[square.y][square.x].occupied = true;
+                self.theMatrix[square.y][square.x].color = block.color;
+            });
+        },
+        renderTheMatrix: function(block) {
+            var self = this,
+                canvas = $('#game canvas')[0],
+                ctx = null,
+                squareSize = this.squaresLength
+                startAtX = undefined,
+                startAtY = undefined;
+
+            if(!canvas.getContext) {
+                console.error('No canvas support!');
+            } else {
+                ctx = canvas.getContext('2d');
+                //clear the canvas
+                ctx.clearRect(0, 0, self.getCanvasWidth(), self.getCanvasHeight());
+                //render the matrix
+                $.each(self.theMatrix, function(rowIndex, row) {
+                    $.each(row, function(cellIndex, cell) {
+                        if(cell.occupied) {
+                            ctx.fillStyle = cell.color;
+                            //calculate coordinates
+                            startAtX = cellIndex * self.getGridUnitLength();
+                            startAtY = rowIndex * self.getGridUnitLength();
+                            ctx.fillRect(startAtX, startAtY, squareSize, squareSize);
+                        }
+                    });
+                });
+                //render the block that is still free
+                $.each(block.squares, function(key, square) {
+                    ctx.fillStyle = block.color;
+                    //calculate coordinates
+                    startAtX = square.x * self.getGridUnitLength();
+                    startAtY = square.y * self.getGridUnitLength();
+                    ctx.fillRect(startAtX, startAtY, squareSize, squareSize);
+                });
+            }
+        }
+    },
+    init: function() {
+        //initialize canvas
+        this.initGameCanvas();
+        //initialize menus
+        this.initMainMenu();
+        this.initInGameMenu();
+        //show starting section
+        $('#splash').show();
+    },
+    initMainMenu: function() {
+        var self = this;
+        //set hover behaviour
+        $('#main-menu li').mouseover(function() {
+            $(this).toggleClass('hover');
+        });
+        $('#main-menu li').mouseout(function() {
+            $(this).toggleClass('hover');
+        });
+        //set buttons behaviour
+        $('#btn-play').click(function() {
+            $('#splash').hide();
+            $('#game').show();
+            self.play();
+        });
+    },
+    initInGameMenu: function() {
+        //set hover behaviour
+        $('#in-game-menu li').mouseover(function() {
+            $(this).toggleClass('hover');
+        });
+        $('#in-game-menu li').mouseout(function() {
+            $(this).toggleClass('hover');
+        });
+        //set buttons behaviour
+        $('#btn-quit').click(function() {
+            $('#game').hide();
+            $('#splash').show();
+        });
+    },
+    initGameCanvas: function() {
+        var canvas = $('#game canvas')[0];
+        canvas.width = this.grid.getCanvasWidth();
+        canvas.height = this.grid.getCanvasHeight();
+    },
+    createBlock: function() {
+        var shape = Math.floor(Math.random()),
+            grid = this.grid,
+            startAtX = (grid.widthInSquares / 2 - 1),
+            block = {
+                squares: [],
+                color: undefined,
+                grounded: false
+            };
+        switch(shape) {
+            case 0:
+                //square
+                block.squares.push({
+                    x: startAtX,
+                    y: 0
+                });
+                block.squares.push({
+                    x: startAtX + 1,
+                    y: 0
+                });
+                block.squares.push({
+                    x: startAtX,
+                    y: 1
+                });
+                block.squares.push({
+                    x: startAtX + 1,
+                    y: 1
+                });
+                block.color = 'rgb(200,0,200)'
+                break;
+            case 1:
+                //column
+                block.squares.push({
+                    x: startAtX,
+                    y: 0
+                });
+                block.squares.push({
+                    x: startAtX,
+                    y: 1
+                });
+                block.squares.push({
+                    x: startAtX,
+                    y: 2
+                });
+                block.squares.push({
+                    x: startAtX,
+                    y: 3
+                });
+                block.color = 'rgb(0,200,0)'
+                break;
+        }
+        if(this.collides(block)) throw 'oh noes!';
+
+        return block;
+    },
+    collides: function(block) {
+        //checks whether the block collides with stuff already in the matrix
+        //@2do: evaluate borders and floor!
+        var self = this;
+        $.each(block.squares, function(key, square) {
+            if(self.grid.theMatrix[square.y][square.x].occupied) {
+                block.grounded = true;
+                return true;
+            }
+        });
+        return false;
+    },
+    move: {
+        left: function(block) {
+            $.each(block.squares, function(key, square) {
+                square.x--;
+            });
+        },
+        right: function(block) {
+            $.each(block.squares, function(key, square) {
+                square.x++;
+            });
+        },
+        down: function(block) {
+            $.each(block.squares, function(key, square) {
+                square.y++;
+            });
+        },
+        drop: function(block) {
+            console.log('drop!');
+        },
+        rotate: function(block) {
+            console.log('rotate!');
+        }
+    },
+    play: function() {
+        var self = this,
+            newBlock = {},
+            gameOver = false,
+            floored = false,
+            chronos = undefined;
+        //initialize game matrix
+        this.grid.initializeTheMatrix();
+        //rock and roll!
+        /* MUST RETHINK
+        while (!gameOver){
+         try {
+            newBlock = this.createBlock();
+            self.grid.renderTheMatrix(newBlock);
+            console.log(newBlock);
+                  console.log('floored i '+floored);
+            while (!floored) {
+                  console.log('floored j '+floored);
+
+                  
+                  
+               chronos = setTimeout(function(){
+                  console.log('floored'+floored);
+                  self.grid.renderTheMatrix(newBlock);
+                  self.move.down(newBlock);
+                  if (self.collides(newBlock)){
+                     floored = true;
+                     self.grid.updateTheMatrix(newBlock); //@do: -1y
+                     clearInterval(chronos);
+                  }
+               }, 1200);
+            }
+            // clearInterval(chronos);
+         } catch (err) {
+            // console.log(err);
+            gameOver = true;
+            //last render
+            self.grid.renderTheMatrix(newBlock);
+            self.gameOverStuff();
+         }
+      }*/
+
+        newBlock = this.createBlock();
+        self.grid.renderTheMatrix(newBlock);
+        //initialize controls
+        $(document).keydown(function(eventObj) {
+            eventObj.preventDefault();
+            console.log(eventObj);
+            switch(eventObj.which) {
+                case 32: //space bar
+                    self.move.drop(newBlock);
+                    break;
+                case 37: //left arrow
+                    self.move.left(newBlock);
+                    break;
+                case 38: //up arrow
+                    self.move.rotate(newBlock);
+                    break;
+                case 39: //right arrow
+                    self.move.right(newBlock);
+                    break;
+                case 40: //down arrow
+                    self.move.down(newBlock);
+                    break;
+            }
+            /* OBSOLETE
+            if(self.collides(newBlock)) {
+                self.grid.updateTheMatrix(newBlock); //@do: -1y
+                console.log(self.grid.theMatrix);
+                floored = true;
+            }*/
+            self.grid.renderTheMatrix(newBlock);
+        });
+        console.log(self.grid.theMatrix);
+    },
+    gameOverStuff: function() {
+        console.error('Game Over!');
+    }
 }
 
 
 $(document).ready(function() {
-   vectris.init();
+    vectris.init();
 })
